8c3162c59b6238eabbef19951e150029ef110e2a
Arquimedes França <arquimedes.2021104590@unicap.br>
2023-03-20 00:02:19 +0000

    Adicionando exemplo de thread

--- src/app/App.java
+++ src/app/App.java
@@ -2,9 +2,12 @@ package app;
 
 public class App {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Oi");
+		MyThread thread = new MyThread();
+		thread.start(); // Start the thread
 	}
 
 }
+
+
---
+++ src/app/MyThread.java
@@ -0,0 +1,8 @@
+package app;
+
+public class MyThread extends Thread {
+    public void run() {
+        // Code to be executed in this thread
+        System.out.println("Hello from MyThread!");
+    }
+}
